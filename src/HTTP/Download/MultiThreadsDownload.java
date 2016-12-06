837219556d51ca16209a27e24e5a912f33ca2bb9
wzy <you@example.com>
2016-12-07 00:33:45 +0800

    laowei 16-12-7 0:33

--- src/HTTP/Download/MultiThreadsDownload.java
+++ src/HTTP/Download/MultiThreadsDownload.java
@@ -1,11 +1,24 @@
 package HTTP.Download;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/12/7.
  */
 public class MultiThreadsDownload implements Runnable {
+    private DownloadFile downloadFile;
+    //record thr position of each file
+    private long[] startPosition;
+    private long[] endPosition;
+    private static final int SLEEP_SECONDS = 500;
+    //threads
+    List<ThreadDownload> threadDownloads = new ArrayList<>();
     @Override
     public void run() {
 
     }
+}
+class ThreadDownload extends Thread {
+
 }
