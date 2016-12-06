19afd78f41898635c5a9d4b6e611eddb5b990a65
wzy <you@example.com>
2016-12-07 00:23:45 +0800

    init commit

--- src/HTTP/Download/DownloadFile.java
+++ src/HTTP/Download/DownloadFile.java
@@ -6,5 +6,36 @@ package HTTP.Download;
 public class DownloadFile {
     private String url;
     private String fileName;
-    
+    private int split;
+    private final static String PATH = "file";
+    private final static int SPLIT_NUM = 5;
+    public DownloadFile(String url, String fileName, int split) {
+        this.url = url;
+        this.fileName = fileName;
+        this.split = split;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getSplit() {
+        return split;
+    }
+
+    public void setSplit(int split) {
+        this.split = split;
+    }
 }
---
+++ src/HTTP/Download/MultiThreadsDownload.java
@@ -0,0 +1,11 @@
+package HTTP.Download;
+
+/**
+ * Created by Administrator on 2016/12/7.
+ */
+public class MultiThreadsDownload implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
